72f42c6d0452f0506f2c871a43e00f34dfd20ea3
felipelopezortega <flopezortega@ucol.mx>
2022-03-09 20:05:37 -0600

    se agrego el archivo app con su codigo correspondiente, queda pendiente su verificacion usando la web

---
+++ app.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer(function(req,res){
+
+    res.writeHead(200, {'Content-Type': 'tex/plain'});
+    res.end('Hello world');
+
+}).listen(1337,'127.0.0.1');
+//Aqui se usa de un paquete para mandar unas response apartir de que se pida algo en el navegador por medio del localhost
